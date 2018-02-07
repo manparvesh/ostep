6c7e748b15543107c5ccbc875418ea132a986105
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2018-02-08 06:57:16 +0800

    add cmake with tests

---
+++ Codes/2/cpu.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <assert.h>
+//#include "common.h"
+
+int main(int argc, char const *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: cpu <string>\n");
+        exit(1);
+    }
+    const char *string = argv[1];
+    while (1) {
+        // Spin(1);
+        printf("%s\n", string);
+    }
+    return 0;
+}
---
+++ Codes/2/cpu_test.c
@@ -0,0 +1,10 @@
+//
+// Created by manparvesh on 8/2/18.
+//
+
+#include <assert.h>
+
+int main(){
+    assert(1);
+    return 0;
+}

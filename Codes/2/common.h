880789dd03b1ea066ae5e0a3c9936a3124b5a92f
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2018-02-08 10:25:58 +0800

    add threads and mem

--- Codes/2/common.h
+++ Codes/2/common.h
@@ -3,6 +3,7 @@
 
 #include <sys/time.h>
 #include <assert.h>
+#include <pthread.h>
 
 double GetTime() {
     struct timeval t;
@@ -17,6 +18,30 @@ void Spin(int howlong) {
 	; // do nothing in loop
 }
 
+void Pthread_create(pthread_t *t, const pthread_attr_t *attr,
+                    void *(*start_routine)(void *), void *arg) {
+    int rc = pthread_create(t, attr, start_routine, arg);
+    assert(rc == 0);
+}
 
+void Pthread_join(pthread_t thread, void **value_ptr) {
+    int rc = pthread_join(thread, value_ptr);
+    assert(rc == 0);
+}
+
+void Pthread_mutex_lock(pthread_mutex_t *mutex) {
+    int rc = pthread_mutex_lock(mutex);
+    assert(rc == 0);
+}
+
+void Pthread_mutex_unlock(pthread_mutex_t *mutex) {
+    int rc = pthread_mutex_unlock(mutex);
+    assert(rc == 0);
+}
+
+void Pthread_mutex_init(pthread_mutex_t *mutex, pthread_mutexattr_t *attr) {
+    int rc = pthread_mutex_init(mutex, attr);
+    assert(rc == 0);
+}
 
 #endif // __common_h__
